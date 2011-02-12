ea1d14107b5a3675de9037df871883c6ebb49ab2
furrymonsta <furrymonsta@c3964249-8a05-c56e-9901-872df67a96b4>
2011-02-12 14:19:32 +0000

    Uploading renamed project

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iphoneLibTest
+//
+//  Created by Richard Dearlove on 23/10/2008.
+//  Copyright DiddySoft 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
